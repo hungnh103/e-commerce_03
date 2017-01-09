fd02175467a7efc8138b27c94285f911ea1847bd
HungNH <hungnh103@gmail.com>
2017-01-09 08:39:37 +0700

    [Admin][Manage Users] View List

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   def index
     @users = User.select(:name, :email, :role).order(created_at: :DESC)
-      .paginate page: params[:page], per_page: Settings.admin.users_list.per_page
+      .page(params[:page]).per Settings.admin.users_list.per_page
   end
 end
