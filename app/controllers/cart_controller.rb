00306a1ea2238fff6b6c400f4e691af547cf10b2
HungNH <hungnh103@gmail.com>
2017-01-11 15:53:44 +0700

    View Cart

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,7 @@
+class CartController < ApplicationController
+  def index
+    @cart = session[:cart] ? session[:cart] : {}
+    @product_carts = @cart.map {|id, quantity|
+      [Product.find_by(id: id), quantity]}
+  end
+end
